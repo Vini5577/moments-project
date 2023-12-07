8eba978430e999d8f782ea0edc3e2f5e9615810c
Hugo Vinícius <ugopessoa@gmail.com>
2023-12-07 09:30:06 -0300

    validation form

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,6 @@
+import { OnInit } from '@angular/core';
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,4 +11,31 @@ import { Component, Input } from '@angular/core';
 export class MomentFormComponent {
 
   @Input() btnText!: string
+
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+
+    console.log('Enviou formulário');
+  }
 }
