fa8fe14e854bb7e32f2a27f3885bc61632458e7a
Hugo Vinícius <ugopessoa@gmail.com>
2023-12-27 11:02:19 -0300

    add remove funcition

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 import { Moment } from 'src/app/Moment';
 import { environment } from 'src/environments/environment';
@@ -21,11 +22,24 @@ export class MomentComponent implements OnInit {
   faTime = faTimes;
   faEdit = faEdit;
 
-  constructor(private momentService: MomentService, private route: ActivatedRoute) { }
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.momentService.getMomentId(id).subscribe(item => this.moment = item.data);
+  }
+
+  async removeHandler(id: number) {
+    await this.momentService.removeMoment(id).subscribe();
+
+    this.messagesService.add("Momento excluído com sucesso!");
+
+    this.router.navigate(['/']);
   }
 }
